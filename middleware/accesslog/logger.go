8329aea1525c2550ab3feeef61916df05d2de06e
fyerfyer <fuy60703@gmail.com>
2025-02-17 15:18:33 +0800

    add option model for server, init logger&opentracing middleware

--- middleware/accesslog/logger.go
+++ middleware/accesslog/logger.go
@@ -1,6 +1,9 @@
-package middleware
+package accesslog
 
-import "github.com/fyerfyer/fyer-webframe/web"
+import (
+	"encoding/json"
+	"github.com/fyerfyer/fyer-webframe/web"
+)
 
 type MiddlewareBuilder struct {
 	logger func(content string)
@@ -30,9 +33,13 @@ func (m *MiddlewareBuilder) Build() web.Middleware {
 	return func(handler web.HandlerFunc) web.HandlerFunc {
 		return func(ctx *web.Context) {
 			info := logInfo{
-				Host: ctx.Req.Host,
-				Route: 
+				Host:       ctx.Req.Host,
+				Route:      ctx.RouteURL,
+				Path:       ctx.Req.URL.Path,
+				HttpMethod: ctx.Req.Method,
 			}
+			val, _ := json.Marshal(info)
+			m.logger(string(val))
 		}
 	}
 }
